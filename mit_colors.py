1170bb42d436a28d9ec81432d274125ff7d23c29
MarkGoldman <goldmanm@mit.edu>
2019-03-10 00:06:44 -0500

    added MIT colors to tools

---
+++ mit_colors.py
@@ -0,0 +1,11 @@
+#this file contains colors used in various projects
+
+from __future__ import division
+
+mit_red = (163/255,31/255,52/255)
+mit_grey = (138/255,139/255,140/255)
+mit_light_grey = (194/255,192/255,191/255)
+mit_black = (0,0,0)
+mit_white = (1,1,1)
+mit_blue = (58/255,96/255,110/255) # not official color
+mit_green = (20/255,49/255,9/255) # not official color
